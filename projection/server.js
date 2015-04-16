81c8c1f894dcf5e935099bb6d2c398efb3994697
Spencer Killen <spleen_387@me.com>
2015-04-16 11:17:15 -0600

    Server set up

--- projection/server.js
+++ projection/server.js
@@ -1,2 +1,12 @@
-var express=require("express");
+var express=require("express"),
+    app=express(),
+    path=require("path");
 
+
+app.use(express.static(__dirname + '/app'));
+app.use("/transcribe", express.static(path.join(__dirname, '../app')));
+
+var server=app.listen(80),
+    io=require("socket.io").listen(server);
+
+console.log("Express server listening on port 80");
